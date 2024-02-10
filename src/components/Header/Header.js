dc9541b2d2bca6c2e0def70b74e9fb672a4a871a
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-02-10 14:21:57 +0430

    Component folder added.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Container, Navbar, Nav } from "react-bootstrap";
+
+import './Header.css';
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from "./App";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import "./index.css";
+
+const container = document.getElementById('root');
+const root = ReactDom.createRoot(container);
+root.render(<App />);
