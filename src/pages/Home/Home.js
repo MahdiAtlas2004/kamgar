60ffcedc7846317608d4a4948561f68c971b48e2
Mahdi Atlas <mahdiatlas2004@gmail.com>
2024-02-11 12:08:03 +0430

    Home page Template added.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 import "./Home.css";
 
 const Home = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
-    <div className="homeContainer">
+    <div className="homeContainer d-flex align-items-center justify-content-center">
       <Container>
-        <Row>
-          <Col>
-            <div>
-              <h1>من حمیدالله کامگار هستم</h1>
-              <h2>نویسنده، تاریخ شناس</h2>
-            </div>
+        <Row className="gy-4">
+          <Col
+            lg={6}
+            className="d-flex align-items-center justify-content-center"
+          >
+            <img
+              src="./assets/images/1.JPG"
+              className="homeImage"
+              data-aos="fade-up"
+            />
           </Col>
-          <Col>
+          <Col
+            lg={6}
+            className="d-flex align-items-center justify-content-center"
+          >
+            <div className="homeTitle text-center" data-aos="fade-right">
+              <h1>من حمیدالله کامگار هستم</h1>
+              <h2 dir="rtl">نویسنده، تاریخ شناس، شاعر...</h2>
+            </div>
           </Col>
         </Row>
       </Container>
